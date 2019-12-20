2640ca348969b53d97801ed932e646822f0d98b8
kudo kotaro <kudo_kotaro@kakaku.com>
2019-12-20 16:35:45 +0900

    step6 ・テストコードからメソッド削除

--- sources/android-ui-tests-basic/app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest2.kt
+++ sources/android-ui-tests-basic/app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest2.kt
@@ -1,8 +1,5 @@
 package com.google.samples.apps.sunflower
 
-
-import android.view.View
-import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -10,10 +7,7 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import org.hamcrest.Description
-import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -49,7 +43,6 @@ class GardenActivityTest2 {
                         2),
                         isDisplayed()))
         navigationMenuItemView.perform(click())
-
 
         // PlantListPage#showPlantDetail(plantName: String): PlantDetailPage
         val appCompatTextView = onView(
@@ -107,22 +100,5 @@ class GardenActivityTest2 {
                                 1),
                         isDisplayed()))
         textView.check(matches(withText("Avocado planted")))
-    }
-
-    private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
     }
 }
